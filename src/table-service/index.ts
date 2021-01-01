833894daa9d2d6c9a192e471e2c2155677332170
Pieter-Jan Drouillon <drouillon@hotmail.com>
2021-01-01 20:36:11 +0100

    add call to material table schematic

--- src/table-service/index.ts
+++ src/table-service/index.ts
@@ -1,10 +1,13 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {chain, externalSchematic, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function tableService(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+    console.log(_options);
+    let tableComponentName = `${_options.entityName.toLowerCase()}list`;
+    return chain([
+      externalSchematic("@angular/material","table",{"name": tableComponentName}),
+      (tree: Tree, _context: SchematicContext) => {return tree}
+    ]);
 }
